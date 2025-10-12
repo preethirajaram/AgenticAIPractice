7f5c0f3deccbe1c09a8b7c00acd1b8e1c0cf3c2b
Preethirajaram <preethi.obli@gmail.com>
2025-10-12 12:43:05 +1100

    Added 2 new files

--- SingleAgentDemo.py
+++ SingleAgentDemo.py
@@ -5,8 +5,7 @@ from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 os.environ[
-    "OPENAI_API_KEY"] = "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"
-
+    "OPENAI_API_KEY"] = ""
 
 async def main():
     model_client = OpenAIChatCompletionClient(model="gpt-4o")
